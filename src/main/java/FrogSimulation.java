f98a10001b4c1d0abd614c7a6e61cfae0b32af22
ribh76 <ribhat@s.sfusd.edu>
2025-04-24 18:24:37 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -33,8 +33,8 @@ public class FrogSimulation
 		/* to be implemented in part (b) */ 
 		double totalpass = 0.0; 
 		for (int i = 0; i<num; i++){
-			if (this.simulate() == true){
-				totalpass++; 
+			if (this.simulate()){
+				totalpass += 1; 
 			}
 		}
 		return totalpass/num;
